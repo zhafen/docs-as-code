0ef4a9b0fc415367fd1637dfc81d53aaba74573b
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-03-24 18:29:58 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my-python-package',
-    version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A Python package for documentation as code.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/my-python-package',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    name="docs-as-code",
+    version="0.1.0",
+    author="Zach Hafen-Saavedra",
+    author_email="z.hafen.saavedra@gmail.com",
+    description="A Python package for documentation as code.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhafen/docs-as-code",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         # List your package dependencies here
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.13",
 )
