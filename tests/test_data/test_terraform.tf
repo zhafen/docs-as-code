d5af5781961ecadc442fcafd6f6217c391a0a80a
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2025-04-07 20:19:31 -0500

    Customized the terraform test to use terraform_data

--- tests/test_data/test_terraform.tf
+++ tests/test_data/test_terraform.tf
@@ -1,32 +1,15 @@
-# Terraform Demo Configuration
-
-# Specify the required Terraform version
-terraform {
-    required_version = ">= 1.0.0"
+variable "revision" {
+  default = 1
 }
 
-# Configure the provider (e.g., AWS)
-provider "aws" {
-    region = "us-east-1"
+resource "terraform_data" "replacement" {
+  input = var.revision
 }
 
-# Create a resource (e.g., an AWS S3 bucket)
-resource "aws_s3_bucket" "demo_bucket" {
-    bucket = "terraform-demo-bucket-${random_id.bucket_id.hex}"
-    acl    = "private"
-
-    tags = {
-        Name        = "TerraformDemoBucket"
-        Environment = "Demo"
-    }
-}
-
-# Generate a random ID for unique bucket naming
-resource "random_id" "bucket_id" {
-    byte_length = 8
-}
-
-# Output the bucket name
-output "bucket_name" {
-    value = aws_s3_bucket.demo_bucket.bucket
+# This resource has no convenient attribute which forces replacement,
+# but can now be replaced by any change to the revision variable value.
+resource "example_database" "test" {
+  lifecycle {
+    replace_triggered_by = [terraform_data.replacement]
+  }
 }
